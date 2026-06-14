0897aa9452989e3ed1c7e60278afe7f59ebe6602
nawapolza <nawapolsirapahtn@gmail.com>
2026-06-14 14:35:35 +0700

    test4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: '0.0.0.0',
     port: 5173,
 
-    allowedHosts: [
-      'oil-management-system.onrender.com'
-    ],
+    allowedHosts: true,
 
     proxy: {
       '/api': {
         target: 'http://localhost/oil-management-system/backend/public/index.php',
         changeOrigin: true,
         secure: false,
+
         rewrite: (path) => {
           const route = path.replace(/^\/api\/?/, '')
           return `?route=/${route}`
